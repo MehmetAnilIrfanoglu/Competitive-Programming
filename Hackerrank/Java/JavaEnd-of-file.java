b19b9c92b8eaa62b397338eb5bceda140f49d9f0
Mehmet Anıl İrfanoğlu <55062643+MehmetAnilIrfanoglu@users.noreply.github.com>
2021-07-27 16:24:19 +0300

    Create JavaEnd-of-file.java

---
+++ Hackerrank/Java/JavaEnd-of-file.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int counter = 0;
+        while (scan.hasNext()){
+            counter++;
+            System.out.println(counter+" "+scan.nextLine());
+        }
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
