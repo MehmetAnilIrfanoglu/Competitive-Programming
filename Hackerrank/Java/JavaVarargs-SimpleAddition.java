3c3b218e5cd4104f74ff22704c34e28b1eb73476
Mehmet Anıl İrfanoğlu <55062643+MehmetAnilIrfanoglu@users.noreply.github.com>
2021-07-27 16:44:00 +0300

    Create JavaVarargs-SimpleAddition.java

---
+++ Hackerrank/Java/JavaVarargs-SimpleAddition.java
@@ -0,0 +1,22 @@
+
+
+class Add {
+    
+    public static void add(int n1,int n2){
+        System.out.println(n1+"+"+n2+"="+(n1+n2));
+    }
+    public static void add(int n1,int n2,int n3){
+        System.out.println(n1+"+"+n2+"+"+n3+"="+(n1+n2+n3));
+    }
+    public static void add(int n1,int n2,int n3,int n4){
+        System.out.println(n1+"+"+n2+"+"+n3+"+"+n4+"="+(n1+n2+n3+n4));
+    }
+    public static void add(int n1,int n2,int n3,int n4,int n5){
+        System.out.println(n1+"+"+n2+"+"+n3+"+"+n4+"+"+n5+"="+(n1+n2+n3+n4+n5));
+    }
+    public static void add(int n1,int n2,int n3,int n4,int n5,int n6){
+        System.out.println(n1+"+"+n2+"+"+n3+"+"+n4+"+"+n5+"+"+n6+"="+(n1+n2+n3+n4+n5+n6));
+    }
+    
+}
+
