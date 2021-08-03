c573f0eb4db7772d4a33a2332960d349d65c84a4
Mehmet Anıl İrfanoğlu <55062643+MehmetAnilIrfanoglu@users.noreply.github.com>
2021-08-03 20:56:49 +0300

    Create Reversealinkedlist.java

---
+++ Hackerrank/Data Structures/Reversealinkedlist.java
@@ -0,0 +1,95 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+class Result {
+
+    /*
+     * Complete the 'reverse' function below.
+     *
+     * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+     * The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+     */
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+
+    
+
+}
+public static SinglyLinkedListNode reverse(SinglyLinkedListNode llist) {
+        SinglyLinkedListNode result;
+        Stack<Integer> st = new Stack<Integer>();
+        while(true){
+            st.push(llist.data);
+            if(llist.next==null)break;
+            llist=llist.next;
+        }
+        result = new SinglyLinkedListNode(st.pop());
+       llist = result;
+        while(st.isEmpty()!=true){
+            result.next= new SinglyLinkedListNode(st.pop());
+            result = result.next;
+        }
+        return llist;
+    // Write your code here
+
+    }
+    private static final Scanner scanner = new Scanner(System.in);
